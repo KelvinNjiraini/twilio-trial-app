95a1788b99f6ea9bf84f6bbe10069a803000e3b3
Kelvin Njiraini <njirainikelvin@gmail.com>
2022-02-10 19:05:31 +0300

    login part works

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 require('dotenv').config();
 const express = require('express');
+const config = require('./config');
 
 const app = express();
+const client = require('twilio')(config.accountSID, config.authToken);
+
+app.get('/login', (req, res) => {
+    client.verify
+        .services(config.serviceId)
+        .verifications.create({
+            to: `+${req.query.phonenumber}`,
+            channel: req.query.channel,
+        })
+        .then((data) => {
+            res.status(200).send(data);
+        });
+});
 
 const port = process.env.PORT;
 app.listen(port, () => {
